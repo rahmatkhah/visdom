995d291a49b84bc2071cf74f09e7ec001eafcecb
Hadi <rahmatkhah@gmail.com>
2017-12-17 10:20:51 -0500

    updated setup.py to reflect the changes

--- setup.py
+++ setup.py
@@ -10,23 +10,25 @@ requirements = [
     'pyzmq',
     'six',
     'torchfile',
+    'plotly',
 ]
 
 setup(
     # Metadata
-    name='visdom',
-    version='0.1.6.4',
-    author='Allan Jabri',
-    author_email='ajabri@fb.com',
-    url='https://github.com/facebookresearch/visdom',
-    description='A tool for visualizing live, rich data for Torch and Numpy',
+    name='visdom_plotly',
+    version='0.1.6.4.1',
+    author='Hadi Rahmat-Khah',
+    author_email='rahmatkhah@gmail.com',
+    url='https://github.com/rahmatkhah/visdom',
+    download_url='https://github.com/rahmatkhah/visdom/archive/0.1.6.4.1.tar.gz',
+    description='Visdom with the plot.ly support',
     long_description=readme,
     license='CC-BY-4.0',
 
     # Package info
-    packages=['visdom'],
-    package_dir={'visdom': 'py'},
-    package_data={'visdom': ['static/*.*', 'static/**/*']},
+    packages=['visdom_plotly'],
+    package_dir={'visdom_plotly': 'py'},
+    package_data={'visdom_plotly': ['static/*.*', 'static/**/*']},
     include_package_data=True,
     zip_safe=False,
     install_requires=requirements,
